7cb2dace2cc010e331774309ef3ed8a8f01f88ef
Cruz Ortiz <cortiz99>
2019-05-12 17:06:41 -0400

    starting middleware training

---
+++ src/02-express-basics/02-middleware.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const port = 3000
+const host = 'localhost'
+const app = express()
+
+// Middleware raíz
+// Middleware get para la segunda pagina
+// Middleware post para redirections
+
+app.listen(port, host)
