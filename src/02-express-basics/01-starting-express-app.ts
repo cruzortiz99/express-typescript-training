ece3afedcdd975c3ed4676e22b6b7fd47f79f769
Cruz Ortiz <cortiz99>
2019-05-12 16:54:26 -0400

    express app started successfuly

--- src/02-express-basics/01-starting-express-app.ts
+++ src/02-express-basics/01-starting-express-app.ts
@@ -1,18 +1,23 @@
 import express from 'express'
-
+// Constantes para la aplicación
 const port = 3000
 const host = 'localhost'
+// Construye una aplicación express
 const app = express()
-
+// Construye un Middleware para la raíz
 app.use('/', (request, response, next) => {
+  console.log(request.method, request.url)
   if (request.method === 'GET') {
-    response.write(`
+    // Usar send para escribir la respuesta y cerrar el Middleware
+    // next() da paso al siguiente Middleware pero no lo cierra
+    // response.end() también lo cierra pero send() es más directo
+    response.send(`
     <h1>Hola mundo!</h1>
     `)
   }
-  next()
 })
-
+// Inicia una aplicación express directamente en un servidor http.
+// También se puede ejecutar con node, pasando la app de express al servidor
 app.listen(port, host, () => {
   console.log(`Running on ${host}:${port}`)
 })
