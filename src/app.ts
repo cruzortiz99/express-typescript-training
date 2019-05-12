5c426446531a549ee3611c7a4e13de5fd33c3a4d
Cruz Ortiz <cortiz99>
2019-05-12 17:47:30 -0400

    main view

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,27 @@
-console.log('Hola mundo!')
+import express from 'express'
+import fs from 'fs'
+import path from 'path'
+
+const port = 3000
+const host = 'localhost'
+const app = express()
+
+app.get('/', (request, response) => {
+  '../static/index.html'
+  let file = fs.createReadStream(
+    path.join(__dirname, '..', 'static', 'index.html'),
+    {
+      encoding: 'utf8'
+    }
+  )
+  file.on('data', (chunk) => {
+    response.write(chunk)
+  })
+  file.on('close', () => {
+    response.end()
+  })
+})
+
+app.listen(port, host, () => {
+  console.log(`Running on ${host}:${port}`)
+})
