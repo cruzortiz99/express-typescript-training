65a12c0c9940a249c66c92200568532f03212a13
Cruz Ortiz <cortiz99>
2019-05-12 21:02:33 -0400

    adding main page and style

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,11 @@ import express from 'express'
 import fs from 'fs'
 import path from 'path'
 
-const port = 3000
+const port = parseInt(`${process.env.PORT || 3000}`)
 const host = 'localhost'
 const app = express()
 
 app.get('/', (request, response) => {
-  '../static/index.html'
   let file = fs.createReadStream(
     path.join(__dirname, '..', 'static', 'index.html'),
     {
