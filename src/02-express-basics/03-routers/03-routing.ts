fcd31061d7491ae33ee02701af20f6dc8a37b4d2
Cruz Ortiz <cortiz99>
2019-05-20 18:51:28 -0400

    starting router lesson

--- src/02-express-basics/03-routers/03-routing.ts
+++ src/02-express-basics/03-routers/03-routing.ts
@@ -3,3 +3,12 @@
  * los routers de express para dividir el código y
  * hacer la aplicación más sostenible
  */
+import express from 'express'
+
+const app = express()
+const port = 3000
+const host = 'localhost'
+
+app.listen(port, host, () => {
+  console.log(`Running on ${host}:${port}`)
+})
