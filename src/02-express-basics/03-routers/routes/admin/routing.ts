c33977c4a6546fc3601ec086967ed4f684867b33
Cruz Ortiz <cortiz99>
2019-05-20 21:31:00 -0400

    documenting

--- src/02-express-basics/03-routers/routes/admin/routing.ts
+++ src/02-express-basics/03-routers/routes/admin/routing.ts
@@ -1,3 +1,7 @@
+/**
+ * Se llama a Router de express que se comporta similar
+ * a una aplicación express
+ */
 import { Router } from 'express'
 import path from 'path'
 import fs from 'fs'
@@ -16,12 +20,16 @@ const adminAssets = [
   'assets',
   'admin'
 ]
-
+/**
+ * se crean las rutas tal cual una aplicación express
+ */
 router.get('/', (request, response) => {
   fs.readFile(path.join(...adminAssets, 'index.html'), (err, data) => {
     response.end(data)
   })
 })
-
+/**
+ * Se exporta el router definido y si aplica su filtro
+ */
 export const adminFilter = '/admin'
 export const adminRouter = router
--- src/02-express-basics/03-routers/routes/user/routing.ts
+++ src/02-express-basics/03-routers/routes/user/routing.ts
@@ -1,3 +1,7 @@
+/**
+ * Se llama a Router de express que se comporta similar
+ * a una aplicación express
+ */
 import { Router } from 'express'
 import path from 'path'
 import fs from 'fs'
@@ -16,12 +20,16 @@ const userAssets = [
   'assets',
   'user'
 ]
-
+/**
+ * se crean las rutas tal cual una aplicación express
+ */
 router.get('/', (request, response) => {
   fs.readFile(path.join(...userAssets, 'index.html'), (err, data) => {
     response.end(data)
   })
 })
-
+/**
+ * Se exporta el router definido y si aplica su filtro
+ */
 export const userFilter = '/user'
 export const userRouter = router
