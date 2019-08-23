846f0046f2b174c88c942895b0f0fd75c1eefb40
Cruz Ortiz <cortiz99>
2019-08-23 04:20:54 -0400

    saving in document database

--- src/03-express-intermediate/01-MVC/entities/User.ts
+++ src/03-express-intermediate/01-MVC/entities/User.ts
@@ -1,5 +1,8 @@
-const users: User[] = []
+import fs from 'fs'
+import path from 'path'
 
+const db = path.resolve(__dirname, '..', 'db')
+const entity = path.resolve(db, 'user.json')
 export class User {
   name: string = ''
   age: number = 0
@@ -9,11 +12,36 @@ export class User {
   }
 
   save = () => {
-    users.push(this)
+    fs.readdir(db, (err) => {
+      const usersJson = JSON.stringify([this])
+      if (err) {
+        return fs.mkdir(db, (err) => {
+          if (err) return console.error(err)
+          fs.writeFile(entity, usersJson, (err) => {
+            if (err) return console.error(err)
+          })
+        })
+      }
+      fs.readFile(entity, (err, data) => {
+        if (err) {
+          return fs.writeFile(entity, usersJson, (err) => {
+            if (err) return console.error(err)
+          })
+        }
+        const usersInDB: User[] = JSON.parse(data.toString())
+        usersInDB.push(this)
+        const savedUsersJson = JSON.stringify(usersInDB)
+        console.log(usersInDB)
+        fs.writeFile(entity, savedUsersJson, (err) => {
+          if (err) return console.error(err)
+        })
+      })
+    })
     return this
   }
 
   static fetchAll = () => {
-    return users
+    const users = fs.readFileSync(entity)
+    return JSON.parse(users.toString())
   }
 }
